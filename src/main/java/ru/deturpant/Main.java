a443b64cd602094f6be1841f639cabc7cbafeaf8
deturpant <vlad3032.kozlov@yandex.ru>
2024-01-02 21:41:39 +0300

    fixed some bugs (fuck java)

--- src/main/java/ru/deturpant/Main.java
+++ src/main/java/ru/deturpant/Main.java
@@ -1,5 +1,6 @@
 package ru.deturpant;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -12,5 +13,28 @@ public class Main {
                 .configure()
                 .addAnnotatedClass(Catalog.class)
                 .buildSessionFactory();
+        Session session = null;
+        try {
+            //CREATE
+/*            session = factory.getCurrentSession();
+            Catalog newCat = new Catalog("Firefox14");
+            session.beginTransaction();
+            session.save(newCat);
+            session.getTransaction().commit();*/
+
+            //READ
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Catalog catalog = session.get(Catalog.class, 2);
+            session.getTransaction().commit();
+            System.out.println(catalog);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            factory.close();
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 }
