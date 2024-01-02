488a0b64caaa40c55398a18503d0f6a781c0c65e
deturpant <vlad3032.kozlov@yandex.ru>
2024-01-02 20:44:05 +0300

    created simple class Catalog.java

---
+++ src/main/java/ru/deturpant/Catalog.java
@@ -0,0 +1,48 @@
+package ru.deturpant;
+
+import org.hibernate.annotations.CollectionId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="catalogs")
+public class Catalog
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+    @Column(name="title")
+    private String title;
+
+    public Catalog(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Catalog{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    public Catalog() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
